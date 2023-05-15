e4213a600705879fc1ff3363ac48f9e9afa12123
davidcoats <david@getsafetycone.com>
2023-05-15 10:18:05 -0700

    Initial commit.

---
+++ source/R5T.L0041/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0041
+{
+	/// <summary>
+	/// Visual Studio solution file context operations library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
